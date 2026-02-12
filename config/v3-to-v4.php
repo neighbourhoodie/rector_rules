7ab4430cf6a29533addace11507f0b710b6aaaae
hulkoba <jacoba@neighbourhood.ie>
2026-02-12 10:31:48 +0100

    feat: handle different rulesets

---
+++ config/v3-to-v4.php
@@ -0,0 +1,10 @@
+<?php
+use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
+
+use phpseclib\rectorRules\Rector\V3toV4\X509;
+
+return RectorConfig::configure()
+  ->withRules([
+    X509::class,
+  ]);
