3d8b3c91d3181d8748c00b5a7ba5effa7766e6a0
hulkoba <jacoba@neighbourhood.ie>
2026-02-19 11:41:18 +0100

    feat: add rule for CRL refactor

--- config/v3-to-v4.php
+++ config/v3-to-v4.php
@@ -3,11 +3,13 @@ use Rector\Config\RectorConfig;
 
 use phpseclib\rectorRules\Rector\V3toV4\HandleFileX509Imports;
 use phpseclib\rectorRules\Rector\V3toV4\X509;
+use phpseclib\rectorRules\Rector\V3toV4\CRL;
 use phpseclib\rectorRules\Rector\V3toV4\SetDNProp;
 
 return RectorConfig::configure()
   ->withRules([
     HandleFileX509Imports::class, // first handle the imports
     X509::class,
-    SetDNProp::class,
+    CRL::class,
+    // SetDNProp::class,
   ]);
